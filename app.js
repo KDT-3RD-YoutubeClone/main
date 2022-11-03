d1823c8ab345708b0d5fb84d2409c2fc1aa605f8
오성인 <dhtjddls12@naver.com>
2022-11-03 17:17:35 +0900

    add youtube api search method

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express();
 const PORT = 8000;
 // axios 요청
 const axios = require('axios');
-// google api key
-const apiKey = 'AIzaSyB5RPkxyIe4yXFove-TwgRn05jMKhR2FYA';
-// googleapis module 요청
+const googleKey = 'AIzaSyB5RPkxyIe4yXFove-TwgRn05jMKhR2FYA'
 const {google} = require('googleapis')
 const youtube = google.youtube({
   version: 'v3',
-  auth: apiKey,
+  auth: googleKey,
 });
+
 
 app.set('view engine', 'ejs'); //view engine 등록
 app.use('/views', express.static(__dirname + '/views'));
@@ -22,9 +21,33 @@ app.use(express.json()); // json형식으로 데이터를 받겠다고 선언
 
 //views 파일 설정
 // req : 요청, res: 응답
-app.get('/',function (req,res) {
+app.get('/',  async function (req,res, next) {
   // res.send('root page');
-  res.render('index'); //views/index.ejs 파일을 찾아서 응답
+  try{
+    const response = await youtube.search.list({
+      part: "snippet",
+      //  q = 검색어 (설정가능);
+      chart: "mostPopular",
+      regionCode: "KR",
+      type: "video",
+      maxResults: "8",
+      order: "viewCount",
+      safeSearch: "strict",
+    })
+    let items = response.data.items
+    console.log(items);
+
+    let trendVideoId = []
+    for (i =0; i< items.length; i++){
+      trendVideoId.push(items[i].id.videoId);
+      // console.log(items);
+    }
+    // res.send(trendVideoId);
+    res.render('index', {trendVideoId : trendVideoId});
+  } catch (err) {
+    next(err);
+  }
+     //views/index.ejs 파일을 찾아서 응답
 });
 // localhost:8000/search로 q(검색어) 에 해당하는 검색 결과 데이터를 보내기
 app.get("/search", async (req, res, next) => {
@@ -32,9 +55,10 @@ app.get("/search", async (req, res, next) => {
     const response = await youtube.search.list({
       part: "snippet",
       //  q = 검색어 (설정가능);
-      q: '인기',
+      q: '신나는 노래',
       regionCode: "KR",
       type: "video",
+      maxResults: "8",
     })
     res.send(response);
   } catch (err) {
@@ -55,7 +79,7 @@ app.get("/trend", async (req, res, next) => {
       order: "viewCount",
       safeSearch: "strict",
     })
-    res.send(response);
+    
   } catch (err) {
     next(err);
   }
